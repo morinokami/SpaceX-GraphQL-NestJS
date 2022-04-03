738e551e0bb11467fdee4aacb8b7ef1adfac6c4d
Shinya Fujino <shf0811@gmail.com>
2022-04-03 18:32:55 +0900

    Update Launch model

--- src/launches/launches.service.ts
+++ src/launches/launches.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { DefaultService, Launch as _Launch } from 'src/generated';
-import { Launch } from './models/launch.model';
+import { DatePrecision, Launch } from './models/launch.model';
 
 @Injectable()
 export class LaunchesService {
@@ -9,8 +9,32 @@ export class LaunchesService {
       id: launch.id,
       flightNumber: launch.flight_number,
       name: launch.name,
+      dateUtc: launch.date_utc,
+      dateUnix: launch.date_unix,
+      dateLocal: launch.date_local,
+      datePrecision: launch.date_precision as DatePrecision,
+      staticFireDateUtc: launch.static_fire_date_utc,
+      staticFireDateUnix: launch.static_fire_date_unix,
+      tbd: launch.tbd,
+      net: launch.net,
+      window: launch.window,
+      success: launch.success,
+      failures: launch.failures,
+      upcoming: launch.upcoming,
+      details: launch.details,
       ships: [],
       capsules: [],
+      links: {
+        patch: launch.links.patch,
+        reddit: launch.links.reddit,
+        flickr: launch.links.flickr,
+        presskit: launch.links.presskit,
+        webcast: launch.links.webcast,
+        youtubeId: launch.links.youtube_id,
+        article: launch.links.article,
+        wikipedia: launch.links.wikipedia,
+      },
+      autoUpdate: launch.auto_update,
       shipIds: launch.ships,
       capsuleIds: launch.capsules,
     };
--- src/launches/models/launch.model.ts
+++ src/launches/models/launch.model.ts
@@ -2,6 +2,86 @@ import { Field, HideField, ID, Int, ObjectType } from '@nestjs/graphql';
 import { Capsule } from 'src/capsules/models/capsule.model';
 import { Ship } from 'src/ships/models/ship.model';
 
+export type DatePrecision =
+  | 'half'
+  | 'quarter'
+  | 'year'
+  | 'month'
+  | 'day'
+  | 'hour';
+
+@ObjectType({ description: 'Failure' })
+class Failure {
+  @Field(() => Int, { nullable: true })
+  time?: number;
+
+  @Field(() => Int, { nullable: true })
+  altitude?: number;
+
+  @Field({ nullable: true })
+  reason?: string;
+}
+
+@ObjectType({ description: 'Patch' })
+class Patch {
+  @Field({ nullable: true })
+  small?: string;
+
+  @Field({ nullable: true })
+  large?: string;
+}
+
+@ObjectType({ description: 'Reddit' })
+class Reddit {
+  @Field({ nullable: true })
+  campaign?: string;
+
+  @Field({ nullable: true })
+  launch?: string;
+
+  @Field({ nullable: true })
+  media?: string;
+
+  @Field({ nullable: true })
+  recovery?: string;
+}
+
+@ObjectType({ description: 'Flickr' })
+class Flickr {
+  @Field(() => [String], { nullable: true })
+  small?: string[];
+
+  @Field(() => [String], { nullable: true })
+  original?: string[];
+}
+
+@ObjectType({ description: 'Link' })
+class Link {
+  @Field(() => Patch)
+  patch: Patch;
+
+  @Field(() => Reddit)
+  reddit: Reddit;
+
+  @Field(() => Flickr)
+  flickr: Flickr;
+
+  @Field({ nullable: true })
+  presskit?: string;
+
+  @Field({ nullable: true })
+  webcast?: string;
+
+  @Field({ nullable: true })
+  youtubeId?: string;
+
+  @Field({ nullable: true })
+  article?: string;
+
+  @Field({ nullable: true })
+  wikipedia?: string;
+}
+
 @ObjectType({ description: 'Launch' })
 export class Launch {
   @Field(() => ID)
@@ -13,11 +93,68 @@ export class Launch {
   @Field()
   name: string;
 
+  @Field()
+  dateUtc: string;
+
+  @Field(() => Int)
+  dateUnix: number;
+
+  @Field()
+  dateLocal: string;
+
+  @Field()
+  datePrecision: DatePrecision;
+
+  @Field({ nullable: true })
+  staticFireDateUtc?: string;
+
+  @Field({ nullable: true })
+  staticFireDateUnix?: number;
+
+  @Field(() => Boolean)
+  tbd: boolean;
+
+  @Field(() => Boolean)
+  net: boolean;
+
+  @Field(() => Int, { nullable: true })
+  window?: number;
+
+  // TODO: rocket
+
+  @Field(() => Boolean, { nullable: true })
+  success?: boolean;
+
+  @Field(() => [Failure])
+  failures: Failure[];
+
+  @Field(() => Boolean)
+  upcoming: boolean;
+
+  @Field({ nullable: true })
+  details?: string;
+
+  // TODO: fairings
+
+  // TODO: crew
+
   @Field(() => [Ship])
   ships: Ship[];
 
   @Field(() => [Capsule])
   capsules: Capsule[];
+
+  // TODO: payloads
+
+  // TODO: launchpad
+
+  // TODO: cores
+
+  @Field(() => Link)
+  links: Link;
+
+  @Field(() => Boolean)
+  autoUpdate: boolean;
 
   @HideField()
   shipIds: string[];
